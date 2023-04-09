3c525cc5b725c9e71c4f39aa9fb1ccae443fdc0b
gagglepod <gagglepod@gmail.com>
2023-04-09 14:55:34 -0500

    Baseline waveform UI using HTML/CSS/JS

---
+++ README.md
@@ -0,0 +1,22 @@
+# OT Waveform Experiment
+
+The goal of this experiment is to create an audio waveform UI that allows the user to click on the waveform and captures the audio playtime in seconds. Each of these clicks will populate an array of timestamps (in seconds), allow the user to give each timestamp a title, and finally display those titles on the screen as a list of potential chapter markers (ordered by seconds ascending).
+
+## UI Requirements
+
+- Clickable Audio Waveform UI
+- Captured of audio playtime seconds
+- Store playtime seconds in an array of timestamps (ordered by seconds ascending)
+- Form to add Titles to each recorded timestamp
+- Cancel button to remove Titles and Timestamps
+- Save button to store final chapters in a Firestore database
+
+## Resources
+
+- [NPM wavesurfer.js](https://www.npmjs.com/package/wavesurfer.js)
+- [CDNJS - font-awesome](https://cdnjs.com/libraries/font-awesome)
+
+## Reference
+
+- [How To Display Audio Waveform In A Website Using HTML, CSS & JS [Part 1]](https://youtu.be/yCmnDWCF8m0)
+- [How To Display Audio Waveform In A Website Using HTML, CSS & JS [Part 2]](https://youtu.be/lrrDDBfgN90)
---
+++ src/main.js
@@ -0,0 +1,73 @@
+let audioTrack = WaveSurfer.create({
+  container: ".audio",
+  waveColor: "#cbcdd7",
+  progressColor: "#bf6c5e",
+  barWidth: 2,
+});
+
+const audioFile = "./assets/audio/wy00-war-yankee-trailer-v2-2020.mp3";
+const audioFileName = audioFile.split("/").pop();
+
+// Get Audio Duration
+// const getDuration = (file) => {
+
+//   }
+
+// const audioDuration = getDuration(audioFile);
+// console.log("duration: ", audioDuration);
+
+audioTrack.load(audioFile);
+const audioDuration = audioTrack.getDuration(audioFile);
+console.log("duration: ", audioDuration);
+
+audioTrack.on("ready", function () {
+  console.log("Download Complete! Ready to Play!");
+});
+
+const trackName = (document.getElementById("track").innerHTML = audioFileName);
+const playBtn = document.querySelector(".play-btn");
+const stopBtn = document.querySelector(".stop-btn");
+const muteBtn = document.querySelector(".mute-btn");
+const volumeSlider = document.querySelector(".volume-slider");
+
+playBtn.addEventListener("click", () => {
+  audioTrack.playPause();
+  if (audioTrack.isPlaying()) {
+    playBtn.classList.add("playing");
+    console.log("Playing...");
+  } else {
+    playBtn.classList.remove("playing");
+    console.log("Paused...");
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+  audioTrack.stop(0);
+  playBtn.classList.remove("playing");
+  console.log("Stopped!");
+});
+
+volumeSlider.addEventListener("mouseup", () => {
+  changeVolume(volumeSlider.value);
+});
+
+const changeVolume = (volume) => {
+  audioTrack.setVolume(volume);
+  if (volume == 0) {
+    muteBtn.classList.add("muted");
+  } else {
+    muteBtn.classList.remove("muted");
+  }
+};
+
+muteBtn.addEventListener("click", () => {
+  if (muteBtn.classList.contains("muted")) {
+    muteBtn.classList.remove("muted");
+    audioTrack.setVolume(0.5);
+    volumeSlider.value = 0.5;
+  } else {
+    audioTrack.setVolume(0);
+    muteBtn.classList.add("muted");
+    volumeSlider.value = 0;
+  }
+});
